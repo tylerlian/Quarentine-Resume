447c0cef869f7b5fc09c2270d02f87797a0b2204
tylerlian <tylerlian@gmail.com>
2020-10-03 12:06:21 -0700

    removed starter files

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    TYLER LIAN
+                </div>
+            </nav>  
+        </>
+    )
+}
+
+export default Navbar
